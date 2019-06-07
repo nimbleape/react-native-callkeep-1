646da6b9516f16795a53b7488eb9796f014ca191
Kyle Kurz <kkurz@digium.com>
2019-06-07 09:57:01 -0700

    Make optional permissions actually optional

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ class RNCallKeep {
   _setupAndroid = async (options) => {
     RNCallKeepModule.setup(options);
 
-    const showAccountAlert = await RNCallKeepModule.checkPhoneAccountPermission(options.additionalPermissions);
+    const showAccountAlert = await RNCallKeepModule.checkPhoneAccountPermission(options.additionalPermissions || []);
     const shouldOpenAccounts = await this._alert(options, showAccountAlert);
 
     if (shouldOpenAccounts) {
