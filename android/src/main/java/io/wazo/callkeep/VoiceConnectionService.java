4629e7919ced5e917ad7ac665a59e510f40b2331
Linus Unnebäck <linus@folkdatorn.se>
2019-12-09 17:41:44 +0000

    Remove dependency on com.facebook.* from non-RN part of Android code

--- android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -40,8 +40,6 @@ import android.util.Log;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 
-import com.facebook.react.HeadlessJsTaskService;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -54,6 +52,7 @@ import java.util.stream.Collectors;
 import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
 import static io.wazo.callkeep.Constants.ACTION_ONGOING_CALL;
 import static io.wazo.callkeep.Constants.ACTION_CHECK_REACHABILITY;
+import static io.wazo.callkeep.Constants.ACTION_WAKE_APP;
 import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
 import static io.wazo.callkeep.Constants.EXTRA_CALL_IDENTIFIER;
 import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
@@ -190,20 +189,12 @@ public class VoiceConnectionService extends ConnectionService {
         return outgoingCallConnection;
     }
 
-    private void wakeUpApplication(String uuid, String identifier, String displayName) {
-        Intent headlessIntent = new Intent(
-            this.getApplicationContext(),
-            RNCallKeepBackgroundMessagingService.class
-        );
-        headlessIntent.putExtra("callUUID", uuid);
-        headlessIntent.putExtra("name", displayName);
-        headlessIntent.putExtra("handle", identifier);
-        Log.d(TAG, "wakeUpApplication: " + uuid + ", identifier : " + identifier + ", displayName:" + displayName);
-
-        ComponentName name = this.getApplicationContext().startService(headlessIntent);
-        if (name != null) {
-          HeadlessJsTaskService.acquireWakeLockNow(this.getApplicationContext());
-        }
+    private void wakeUpApplication(String uuid, String number, String displayName) {
+        HashMap<String, String> extrasMap = new HashMap();
+        extrasMap.put(EXTRA_CALL_UUID, uuid);
+        extrasMap.put(EXTRA_CALLER_NAME, displayName);
+        extrasMap.put(EXTRA_CALL_NUMBER, number);
+        sendCallRequestToActivity(ACTION_WAKE_APP, extrasMap);
     }
 
     private void wakeUpAfterReachabilityTimeout(ConnectionRequest request) {
