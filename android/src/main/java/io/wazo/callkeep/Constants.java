react-native-webrtc/react-native-callkeep

Remove dependency on RN* part from other part of Android code

android/src/main/java/io/wazo/callkeep/Constants.java
@@ -1,0 +1,19 @@
+package io.wazo.callkeep;
+
+public class Constants {
+    public static final String CHECKING_PERMS = "CHECKING_PERMS";
+    public static final String EXTRA_CALLER_NAME = "EXTRA_CALLER_NAME";
+    public static final String EXTRA_CALL_UUID = "EXTRA_CALL_UUID";
+    public static final String EXTRA_CALL_NUMBER = "EXTRA_CALL_NUMBER";
+    public static final String ACTION_END_CALL = "ACTION_END_CALL";
+    public static final String ACTION_ANSWER_CALL = "ACTION_ANSWER_CALL";
+    public static final String ACTION_MUTE_CALL = "ACTION_MUTE_CALL";
+    public static final String ACTION_UNMUTE_CALL = "ACTION_UNMUTE_CALL";
+    public static final String ACTION_DTMF_TONE = "ACTION_DTMF_TONE";
+    public static final String ACTION_HOLD_CALL = "ACTION_HOLD_CALL";
+    public static final String ACTION_UNHOLD_CALL = "ACTION_UNHOLD_CALL";
+    public static final String ACTION_ONGOING_CALL = "ACTION_ONGOING_CALL";
+    public static final String ACTION_AUDIO_SESSION = "ACTION_AUDIO_SESSION";
+    public static final String ACTION_CHECK_REACHABILITY = "ACTION_CHECK_REACHABILITY";
+    public static final String ACTION_WAKE_APP = "ACTION_WAKE_APP";
+}

android/src/main/java/io/wazo/callkeep/RNCallKeepBackgroundMessagingService.java
@@ -26,9 +26,9 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_NUMBER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_NUMBER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 import javax.annotation.Nullable;
 

android/src/main/java/io/wazo/callkeep/VoiceConnection.java
@@ -37,17 +37,17 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ANSWER_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_AUDIO_SESSION;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_DTMF_TONE;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_END_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_HOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_MUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNHOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNMUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_NUMBER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
+import static io.wazo.callkeep.Constants.ACTION_ANSWER_CALL;
+import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
+import static io.wazo.callkeep.Constants.ACTION_DTMF_TONE;
+import static io.wazo.callkeep.Constants.ACTION_END_CALL;
+import static io.wazo.callkeep.Constants.ACTION_HOLD_CALL;
+import static io.wazo.callkeep.Constants.ACTION_MUTE_CALL;
+import static io.wazo.callkeep.Constants.ACTION_UNHOLD_CALL;
+import static io.wazo.callkeep.Constants.ACTION_UNMUTE_CALL;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_NUMBER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 @TargetApi(Build.VERSION_CODES.M)
 public class VoiceConnection extends Connection {

android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -53,20 +53,12 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ANSWER_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_AUDIO_SESSION;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_DTMF_TONE;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_END_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_HOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_MUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ONGOING_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNHOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNMUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_CHECK_REACHABILITY;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_NUMBER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
-import static io.wazo.callkeep.RNCallKeepModule.handle;
+import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
+import static io.wazo.callkeep.Constants.ACTION_ONGOING_CALL;
+import static io.wazo.callkeep.Constants.ACTION_CHECK_REACHABILITY;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_NUMBER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 // @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionService.java
 @TargetApi(Build.VERSION_CODES.M)
@@ -76,6 +68,7 @@ public class VoiceConnectionService extends ConnectionService {
     private static Boolean isReachable;
     private static String notReachableCallUuid;
     private static ConnectionRequest currentConnectionRequest;
+    private static PhoneAccountHandle phoneAccountHandle;
     private static String TAG = "RNCK:VoiceConnectionService";
     public static Map<String, VoiceConnection> currentConnections = new HashMap<>();
     public static Boolean hasOutgoingCall = false;
@@ -96,6 +89,10 @@ public class VoiceConnectionService extends ConnectionService {
         isAvailable = false;
         currentConnectionRequest = null;
         currentConnectionService = this;
+    }
+
+    public static void setPhoneAccountHandle(PhoneAccountHandle phoneAccountHandle) {
+        VoiceConnectionService.phoneAccountHandle = phoneAccountHandle;
     }
 
     public static void setAvailable(Boolean value) {
@@ -271,9 +268,7 @@ public class VoiceConnectionService extends ConnectionService {
         VoiceConnection voiceConnection1 = (VoiceConnection) connection1;
         VoiceConnection voiceConnection2 = (VoiceConnection) connection2;
 
-        PhoneAccountHandle phoneAccountHandle = RNCallKeepModule.handle;
-
-        VoiceConference voiceConference = new VoiceConference(handle);
+        VoiceConference voiceConference = new VoiceConference(phoneAccountHandle);
         voiceConference.addConnection(voiceConnection1);
         voiceConference.addConnection(voiceConnection2);
 

nimbleape/react-native-callkeep-1

android/src/main/java/io/wazo/callkeep/Constants.java
@@ -1,0 +1,19 @@
+package io.wazo.callkeep;
+
+public class Constants {
+    public static final String CHECKING_PERMS = "CHECKING_PERMS";
+    public static final String EXTRA_CALLER_NAME = "EXTRA_CALLER_NAME";
+    public static final String EXTRA_CALL_UUID = "EXTRA_CALL_UUID";
+    public static final String EXTRA_CALL_IDENTIFIER = "EXTRA_CALL_NUMBER";
+    public static final String ACTION_END_CALL = "ACTION_END_CALL";
+    public static final String ACTION_ANSWER_CALL = "ACTION_ANSWER_CALL";
+    public static final String ACTION_MUTE_CALL = "ACTION_MUTE_CALL";
+    public static final String ACTION_UNMUTE_CALL = "ACTION_UNMUTE_CALL";
+    public static final String ACTION_DTMF_TONE = "ACTION_DTMF_TONE";
+    public static final String ACTION_HOLD_CALL = "ACTION_HOLD_CALL";
+    public static final String ACTION_UNHOLD_CALL = "ACTION_UNHOLD_CALL";
+    public static final String ACTION_ONGOING_CALL = "ACTION_ONGOING_CALL";
+    public static final String ACTION_AUDIO_SESSION = "ACTION_AUDIO_SESSION";
+    public static final String ACTION_CHECK_REACHABILITY = "ACTION_CHECK_REACHABILITY";
+    public static final String ACTION_WAKE_APP = "ACTION_WAKE_APP";
+}

android/src/main/java/io/wazo/callkeep/RNCallKeepBackgroundMessagingService.java
@@ -26,9 +26,9 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_IDENTIFIER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_IDENTIFIER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 import javax.annotation.Nullable;
 

android/src/main/java/io/wazo/callkeep/VoiceConnection.java
@@ -37,17 +37,17 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ANSWER_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_AUDIO_SESSION;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_DTMF_TONE;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_END_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_HOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_MUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNHOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNMUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_IDENTIFIER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
+import static io.wazo.callkeep.Constants.ACTION_ANSWER_CALL;
+import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
+import static io.wazo.callkeep.Constants.ACTION_DTMF_TONE;
+import static io.wazo.callkeep.Constants.ACTION_END_CALL;
+import static io.wazo.callkeep.Constants.ACTION_HOLD_CALL;
+import static io.wazo.callkeep.Constants.ACTION_MUTE_CALL;
+import static io.wazo.callkeep.Constants.ACTION_UNHOLD_CALL;
+import static io.wazo.callkeep.Constants.ACTION_UNMUTE_CALL;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_IDENTIFIER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 @TargetApi(Build.VERSION_CODES.M)
 public class VoiceConnection extends Connection {

android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -53,20 +53,12 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ANSWER_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_AUDIO_SESSION;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_DTMF_TONE;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_END_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_HOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_MUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_ONGOING_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNHOLD_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_UNMUTE_CALL;
-import static io.wazo.callkeep.RNCallKeepModule.ACTION_CHECK_REACHABILITY;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALLER_NAME;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_IDENTIFIER;
-import static io.wazo.callkeep.RNCallKeepModule.EXTRA_CALL_UUID;
-import static io.wazo.callkeep.RNCallKeepModule.handle;
+import static io.wazo.callkeep.Constants.ACTION_AUDIO_SESSION;
+import static io.wazo.callkeep.Constants.ACTION_ONGOING_CALL;
+import static io.wazo.callkeep.Constants.ACTION_CHECK_REACHABILITY;
+import static io.wazo.callkeep.Constants.EXTRA_CALLER_NAME;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_IDENTIFIER;
+import static io.wazo.callkeep.Constants.EXTRA_CALL_UUID;
 
 // @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionService.java
 @TargetApi(Build.VERSION_CODES.M)
@@ -76,6 +68,7 @@ public class VoiceConnectionService extends ConnectionService {
     private static Boolean isReachable;
     private static String notReachableCallUuid;
     private static ConnectionRequest currentConnectionRequest;
+    private static PhoneAccountHandle phoneAccountHandle;
     private static String TAG = "RNCK:VoiceConnectionService";
     public static Map<String, VoiceConnection> currentConnections = new HashMap<>();
     public static Boolean hasOutgoingCall = false;
@@ -96,6 +89,10 @@ public class VoiceConnectionService extends ConnectionService {
         isAvailable = false;
         currentConnectionRequest = null;
         currentConnectionService = this;
+    }
+
+    public static void setPhoneAccountHandle(PhoneAccountHandle phoneAccountHandle) {
+        VoiceConnectionService.phoneAccountHandle = phoneAccountHandle;
     }
 
     public static void setAvailable(Boolean value) {
@@ -271,9 +268,7 @@ public class VoiceConnectionService extends ConnectionService {
         VoiceConnection voiceConnection1 = (VoiceConnection) connection1;
         VoiceConnection voiceConnection2 = (VoiceConnection) connection2;
 
-        PhoneAccountHandle phoneAccountHandle = RNCallKeepModule.handle;
-
-        VoiceConference voiceConference = new VoiceConference(handle);
+        VoiceConference voiceConference = new VoiceConference(phoneAccountHandle);
         voiceConference.addConnection(voiceConnection1);
         voiceConference.addConnection(voiceConnection2);
 